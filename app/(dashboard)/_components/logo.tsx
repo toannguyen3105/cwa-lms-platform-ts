0aabaa14d943799e92ff7aef86af09833c57e5dd
Toan Nguyen <toannh@nal.vn>
2025-01-06 17:41:18 +0700

    Add basic form
    Add basic form

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return <Image height={130} width={130} alt="Logo" src="/logo.svg" />;
+  return (
+    <div style={{ width: "auto", height: "auto" }}>
+      <Image height={130} width={130} alt="Logo" src="/logo.svg" />
+    </div>
+  );
 };
 
 export default Logo;
